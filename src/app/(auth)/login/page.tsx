3bb34984d374ff0701066c05e6c3b223c8b82c8e
Bryium <bryiumonyancha@gmail.com>
2025-03-07 16:51:15 +0300

    add login and register forms

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
   return <div>LoginPage</div>;
+  return (
+    <div className="flex items-center justify-center vertical-center">
+      <LoginForm />
+    </div>
+  );
 }
